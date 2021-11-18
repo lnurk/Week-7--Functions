5bebcea1f5d95b50c38162601941818f41865503
Lisanne Nurk <lnurk@tktk.ee>
2021-11-18 12:05:33 +0200

    add new projects

---
+++ Functions/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sisesta esimest arvu:");
+            int userNumberOne = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Sisesta teist arvu:");
+            int userNumberTwo = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Vali tehe (sisesta '+', '-', '*' või '/')");
+            char userOperator = Convert.ToChar(Console.ReadLine());
+
+            switch (userOperator)
+            {
+                case '+':
+                    AddTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                case '/':
+                    DivideTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                case '*':
+                    MultiplyTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                case '-':
+                    SubtractTwoNumbers(userNumberOne, userNumberTwo);
+                    break;
+                default:
+                    Console.WriteLine("Kena päeva!");
+                    break;
+            }
+
+        }
+
+        public static void AddTwoNumbers(int x, int y)
+        {
+           int result = x + y;
+            Console.WriteLine(result);
+        }
+
+        public static void DivideTwoNumbers(double x, double y)
+        {
+            double result = x / y;
+            Console.WriteLine(result);
+        }
+
+        public static void MultiplyTwoNumbers(double x, double y)
+        {
+            double result = x * y;
+            Console.WriteLine(result);
+        }
+        public static void SubtractTwoNumbers(int x, int y)
+        {
+            int result = x - y;
+            Console.WriteLine(result);
+        }
+    }
+}
